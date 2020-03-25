40e5e8d0088efd66646d5adb3a98c8b29148c3bb
isaac <isw9@case.edu>
2020-03-25 14:47:41 -0400

    remove secret config

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ class Config(object):
     TOKEN_SECRET_KEY = 'alecisgod'
     MONGO_DBNAME = 'MLAI'
     MONGO_URI = 'mongodb://localhost:27017/MLAI'
-    SENDGRID_API_KEY = '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'
+    SENDGRID_API_KEY = 'tbd'
