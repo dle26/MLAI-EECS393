b71d04ee00b9d4f86f238b36e4523242f404e050
James Anibal <james.anibal@case.edu>
2020-03-27 22:11:35 -0400

    just for scoring now

--- ml_backend/AbstractAwk/abstractawk.sh
+++ ml_backend/AbstractAwk/abstractawk.sh
@@ -1,56 +1,12 @@
 #!/bin/bash
 
 args=("$@")
-word=${args[0]} 
-year1=${args[1]}  
-year2=${args[2]}  
-paper=${args[3]}  
-related=${args[4]}  
-user_id=${args[5]}  
+file = ${args[0]} 
 
-none="None"
-numyears=$((year2-year1))
-k=0
-
-for ((i=0; i<=$numyears;i++))
-do  
-  lynx --dump --nolist "https://www.ncbi.nlm.nih.gov/pubmed/?term=$word+AND+$((year1+i))[pdat]&dispmax=$papers&report=abstract"> "output$i.txt"
-  if [ $related != $none ]
-  then
-     lynx -dump "https://www.ncbi.nlm.nih.gov/pubmed/?term=$word+AND+$((year1+i))[pdat]&dispmax=$papers&report=abstract" | awk '/http/{print $2}' > "links$i$user_id"
-     sed -i '/linkname=pubmed_pubmed&from_uid/!d' "links$i.txt"
-     mapfile -t arr < "links$i$user_id.txt"
-     len=${#arr[@]}
-     rm "links$i$user_id"
-     for j in ${arr[*]}
-     do 
-       k=$((k+1))
-       lynx --dump --nolist "$j&dispmax=$related&report=abstract" > "related$k$user_id.txt"
-     done
-  fi
-done
-
-cat *txt > testfile 
-
-for ((i=0;i<=$numyears;i++))
-do
-   rm "output$i$user_id.txt"
-done
-
-
-if [ $related != $none ]
-then
-  for ((i=0;i<=$k;i++))
-  do
-   rm "related$i$user_id.txt"
-  done
-fi
-
-
-sed -i 's/[ \t]*//' testfile
-sed -i '/^[[:space:]]*$/d' testfile
+sed -i 's/[ \t]*//' file
+sed -i '/^[[:space:]]*$/d' file
 awk 'length($0)>50' testfile > out
-rm testfile 
+rm file 
 sed -i 's/[0-9]\w\+//g' out
 sed -i 's/[]/.:;<>!=+?,"&@%()[^*]//g' out
 sed -i 's/\\//g' out
