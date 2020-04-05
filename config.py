1e6fbf8c5d1adcaa8b0ead4fd181a4fb471502f7
isaac <isw9@case.edu>
2020-04-05 16:06:58 -0400

    fix email feature

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ class Config(object):
     TOKEN_SECRET_KEY = 'alecisgod'
     MONGO_DBNAME = 'MLAI'
     MONGO_URI = 'mongodb://localhost:27017/MLAI'
-    SENDGRID_API_KEY = 'tbd'
+    SENDGRID_API_KEY = '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'
