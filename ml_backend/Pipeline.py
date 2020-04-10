9f5376ffba2d7ceca39b8de4b82c8572236e02e7
James Anibal <james.anibal@case.edu>
2020-04-09 22:45:07 -0400

    todos

--- ml_backend/Pipeline.py
+++ ml_backend/Pipeline.py
@@ -6,6 +6,18 @@
 
 class Pipeline:
     
+
+    def __init__(data,labels,information):
+        
+        ### list of files data type?
+        ### single file data type?
+        #### where are the labels being stored 
+                  # - we need to define a protocol for the user to upload labels...
+        #### where is the information on the data, the time constraint, and user info?
+        
+        pass 
+    
+    
     def run_MLAI(data):
         pass
 
