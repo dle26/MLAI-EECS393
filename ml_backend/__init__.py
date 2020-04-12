6e3a248367c25cad2c1a296ed0768577fd82e18a
janibal54 <james.anibal@case.edu>
2020-04-12 00:35:37 -0400

    packaged ml_backend

---
+++ ml_backend/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 12 00:30:01 2020
+
+@author: anibaljt
+"""
+
+from .Prep import DATAPREP,DATA
+from .AnalysisAgent import ANALYZE
+from .Pipeline import Pipeline
+from .Interpret import INTERPRET
+from .SelectionAgent import SELECT
+from .TextMine import TEXTMINE
+from .TextProcess import TEXTPROCESS
+from .UniversalScores import UniversalScores
+from .UpdateAgent import UpdateAgent
