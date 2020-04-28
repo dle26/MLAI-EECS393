516845cc1c7614abc7916cdd844cd628cf803789
long <justinphan3110@gmail.com>
2020-04-28 00:33:33 -0400

    lower case devname

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ def user():
 
 @app.route("/devuserinfo", methods=['POST'])
 def devuser():
-    devname = request.get_json(force = True)['devname']
+    devname = request.get_json(force = True)['Devname']
 
     devs = mongodb.db.devs
     dev = devs.find_one({'devname': devname})
@@ -221,7 +221,7 @@ def developer_register():
 
 @app.route("/dev/login", methods=['POST'])
 def dev_login():
-    devname = request.get_json(force = True)['Devname']
+    devname = request.get_json(force = True)['devname']
     password = request.get_json(force = True)['password']
 
     devs = mongodb.db.devs
