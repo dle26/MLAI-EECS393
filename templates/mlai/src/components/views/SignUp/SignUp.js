28a625579fefae781ba0742152d18227549fbb73
James Anibal <james.anibal@case.edu>
2020-05-04 01:55:38 -0400

    port

--- templates/mlai/src/components/views/SignUp/SignUp.js
+++ templates/mlai/src/components/views/SignUp/SignUp.js
@@ -65,7 +65,7 @@ class SignUp extends Component {
 
   signUp() {
     axios
-      .post("http://34.70.151.69:5000/register", {
+      .post("http://34.70.151.69:3110/register", {
         username: this.state.username,
         password: this.state.password,
         firstname: this.state.firstname,
