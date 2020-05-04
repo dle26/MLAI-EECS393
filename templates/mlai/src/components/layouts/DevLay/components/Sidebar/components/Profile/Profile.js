51464b6bd970fb0961b7287b54068bbacd0b373b
James Anibal <james.anibal@case.edu>
2020-05-04 02:01:38 -0400

    ports

--- templates/mlai/src/components/layouts/DevLay/components/Sidebar/components/Profile/Profile.js
+++ templates/mlai/src/components/layouts/DevLay/components/Sidebar/components/Profile/Profile.js
@@ -35,7 +35,7 @@ class Profile extends Component {
 
     if(this.state.devname != null) {
       axios
-      .post("http://34.70.151.69:5000/devuserinfo", {
+      .post("http://34.70.151.69:3110/devuserinfo", {
         "Devname": this.state.devname
       })
       .then(response => {
